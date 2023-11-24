9a2ea29cdd3693d186443ac06b968eb3430bae67
akaryaka <a.karyaka.2dit@gmail.com>
2023-11-25 01:41:26 +0600

    ref

--- src/js/render.js
+++ src/js/render.js
@@ -1,12 +1,15 @@
 export default function render() {
   const app = document.getElementById('app');
 
-
   const form = `
-    <form class="form" action="#">
-      <input type="text" id="message-text"/>
-      <button id="add-message" type="submit">Отправить<button/>
-    </form>
+    <div class="wrapper">
+      <div class="chat"></div>
+      <form class="form" action="#">
+        <input type="text" class="input" id="input-name" placeholder="Ваше имя"/>
+        <input type="text" class="input" id="input-message" placeholder="Введите сообщение" />
+        <button class="btn" id="add-message" type="submit">Отправить<button/>
+      </form>
+    </div>
   `
 
   app.innerHTML = form;
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,18 +2,19 @@ export default function scripts() {
   const addMessage = document.getElementById('add-message');
 
   function sendMessage(event) {
-    const input = document.getElementById('message-text');
-    const messageText = input.value;
+    const inputMessage = document.getElementById('input-message');
+    const inputName = document.getElementById('input-name');
+    const messageText = inputMessage.value;
+    const nameText = inputName.value;
 
     event.preventDefault();
 
     let data = {
       id: '111',
+      name: nameText,
       time: Date.now(),
       text: messageText
     }
-
-    alert(messageText)
   }
   
   addMessage.addEventListener('click', sendMessage); 
